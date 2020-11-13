494bd68bb1cd3d24649b488cd86d74c38de70d3f
Lu-May <1054998345@qq.com>
2020-11-13 17:37:46 +0800

    [luyuehuan] feat:implement practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+    for (let index = 0; index < sections.length; index++) {
+        items.splice(sections[index].index + index, 0, sections[index].content)
+    }
+    return items
+}
 export { inject };
