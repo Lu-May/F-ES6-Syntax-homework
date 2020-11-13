cf0992a13223300571b6ee34bb469e055fa9be95
Lu-May <1054998345@qq.com>
2020-11-13 17:25:31 +0800

    [luyuehuan] feat:implement practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = (input) => {
+  const data = [ ...input.data ]
+  const column = [ ...input.column ]
+  const result = []
+  for (const dataItem of data) {
+    let expectObiect = {}
+    let index = 0
+    for (const columnItem of column) {
+      expectObiect[columnItem.name] = dataItem[index]
+      index++
+    }
+    result.push(expectObiect)
+  }
+  return result;
+}
 export { parseData };
